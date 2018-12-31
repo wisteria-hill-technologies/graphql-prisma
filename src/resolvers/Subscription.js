527ac90b458cff5a9cbd5c3ac0863a8610e6711e
Noby Fujioka <n.fujioka@abacusmaths.info>
2018-12-31 02:28:38 +0000

    working on subscription

--- src/resolvers/Subscription.js
+++ src/resolvers/Subscription.js
@@ -1,30 +1,28 @@
 const Subscription = {
-  // count: {
-  //   subscribe(parent, args, { pubsub }, info) {
-  //     let count = 0;
-  //     setInterval(() => {
-  //       count++;
-  //       pubsub.publish('count', {
-  //         count
-  //       })
-  //     }, 1000);
-  //     return pubsub.asyncIterator('count');
-  //   }
-  // },
-  post: {
-    subscribe(parent, args, { db, pubsub }, info) {
-      return pubsub.asyncIterator('post');
+  comment: {
+    subscribe(parent, { postId }, { prisma }, info){
+      return prisma.subscription.comment({
+        where: {
+          node: {
+            post: {
+              id: postId
+            }
+          }
+        }
+      }, info)
     }
   },
-  comment: {
-    subscribe(parent, { postId }, { db, pubsub }, info) {
-      const post = db.posts.find(post => post.id === postId && post.published);
-      if (!post) {
-        throw new Error('Post not found.');
-      }
-      return pubsub.asyncIterator(`comment ${postId}`);
+  post: {
+    subscribe(parent, args, { prisma }, info) {
+      return prisma.subscription.post({
+        where: {
+          node: {
+            published: true
+          }
+        }
+      }, info)
     }
   }
-};
+}
 
-export { Subscription as default };
+export { Subscription as default }
